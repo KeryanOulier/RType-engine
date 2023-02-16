da4d3a16630d9d34333783b092268e26c8ed27cc
Antoine Rondeau <antoine.rondeau@epitech.eu>
2023-02-16 16:10:55 +0100

    fix(libraries): put the lib_entrypoint into the registry instead of a class

--- includes/Registry.hpp
+++ includes/Registry.hpp
@@ -2,6 +2,9 @@
 #include <functional>
 #include <typeindex>
 #include <unordered_map>
+#include <dlfcn.h>
+#include <filesystem>
+#include <iostream>
 
 #include "Entity.hpp"
 #include "Sparse_array.hpp"
@@ -142,6 +145,29 @@ namespace ecs {
             }
         }
 
+        using entrypoint_fcn = void (*)(ecs::registry &);
+
+        /**
+            @brief: Load a library and execute the entrypoint function
+            @param lib_name: The name of the library to load
+            @param reg: The registry to pass to the entrypoint function
+            @param function_name: The name of the entrypoint function, default is "entrypoint"
+        */
+        void lib_entrypoint(const std::string &lib_name, const std::string &function_name="entrypoint")
+        {
+            auto handle = load_lib(lib_name);
+            if (!handle) {
+                return;
+            }
+            try {
+                auto entrypoint = get_function<entrypoint_fcn>(function_name, handle);
+                entrypoint(*this);
+            } catch (const std::exception &e) {
+                std::cerr << "Error while executing lib_entrypoint: " << e.what() << std::endl;
+                close_lib(handle);
+            }
+        }
+
     private:
         std::unordered_map<std::type_index, std::any> _components_array;
 
@@ -149,5 +175,40 @@ namespace ecs {
         std::vector<size_t> _available_ids;
         std::vector<std::function<void(registry &, entity const &)>>
             _remove_component_functions;
+
+        void *load_lib(const std::string &lib_path)
+        {
+            if (!std::filesystem::exists(lib_path)) {
+                std::cerr << "Cannot find library: " << lib_path << std::endl;
+                return nullptr;
+            }
+
+            auto handle = dlopen(lib_path.c_str(), RTLD_LAZY);
+
+            if (!handle) {
+                std::cerr << "Cannot open library: " << dlerror() << std::endl;
+                return nullptr;
+            }
+            return handle;
+        }
+
+        void close_lib(void *handle)
+        {
+            if (handle)
+                dlclose(handle);
+        }
+
+        template <typename T>
+        T get_function(const std::string &function_name, void *handle)
+        {
+            T function = (T) dlsym(handle, function_name.c_str());
+            const char *dlsym_error = dlerror();
+
+            if (dlsym_error) {
+                std::cerr << "Cannot load symbol '" << function_name << "': " << dlsym_error << std::endl;
+                throw std::runtime_error("Cannot load symbol");
+            }
+            return function;
+        }
     };
 }
